d2554c8025d78c7c70982a78eb05438172dbe39c
Asef Ahmed <asefahmed500@gmail.com>
2024-09-29 20:59:05 +0600

    add

---
+++ server.js
@@ -0,0 +1,70 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cors());
+
+
+// Construct the MongoDB URI using environment variables
+const user = process.env.MONGO_USER;
+const password = process.env.MONGO_PASSWORD;
+const uri = `mongodb+srv://${user}:${password}@cluster0.8vksczm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+// Connect the client to the server
+client.connect().then(() => {
+    const database = client.db("insertDB");
+    const contactCollection = database.collection("contactMessages");
+
+    // Define the route to handle form submissions
+    app.post('/submitContactForm', async (req, res) => {
+        const { name, email, message } = req.body;
+
+        // Insert the message into the collection
+        const result = await contactCollection.insertOne({
+            name,
+            email,
+     
+            message,
+            date: new Date()
+        });
+
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        res.status(200).json({ message: 'Message saved successfully!' });
+    });
+
+
+    // Send a ping to confirm a successful connection
+    client.db("admin").command({ ping: 1 }).then(() => {
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }).catch(console.dir);
+
+}).catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
